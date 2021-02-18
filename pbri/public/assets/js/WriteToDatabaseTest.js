3604371904f5c23dc374801a5b549d7b77938ba5
Caleb <caleb7hayes@gmail.com>
2021-02-18 16:51:04 -0700

    Updating CRUD

--- pbri/public/assets/js/WriteToDatabaseTest.js
+++ pbri/public/assets/js/WriteToDatabaseTest.js
@@ -1,8 +1,45 @@
 var database = firebase.database()
 
-function writeData(key, first, last){
-    firebase.database().ref('keys/' + key).set({
+// Crud functions
+
+function createData(key, first, last){
+    database.ref('keys/' + key).set({
         firstName: first,
-        lastName: last,
+        lastName: last
     });
+}
+
+function readData(key){
+    database.child("users").child(userId).get().then(function(snapshot) {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        }
+        else {
+          console.log("No data available");
+        }
+      }).catch(function(error) {
+        console.error(error);
+      });
+}
+
+function updateData(key, first, last){
+    // Update Data
+    var updatedData = {
+        firstName: first,
+        lastName: last
+    };
+
+    // Get Key
+    var newPostKey = database.ref().child('posts').push().key;
+
+    // Build updates array
+    var updates = {};
+    updates['/posts/' + newPostKey] = updatedData
+    updates['/user-posts/' + key + '/' + newPostKey] = updatedData;
+
+    return database.ref().update(updates)
+}
+
+function deleteData(key){
+    database.ref('keys/' + key).remove();
 }
