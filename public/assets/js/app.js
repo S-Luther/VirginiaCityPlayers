6933964b0c04bcf00ad0840ca435dc6baca18065
Nicholas <nicholaszetterberg@gmail.com>
2021-03-03 09:44:27 -0700

    NoUpdateFunc
    Only thing I am missing from this is adding an edit function, and completing the showList ref id.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,63 @@
 
-const db = firebase.firestore();
-const form = document.querySelector('#add-show-form');
 
-//saving data
+
+const db = firebase.firestore();
+
+const form = document.querySelector('#add-show-form')
+const showList = document.querySelector('#show-list');
+
+function renderList(doc){
+
+    let li = document.createElement('li');
+    let showName = document.createElement('spanner');
+    let showDate = document.createElement('spanner');
+    let editTickets = document.createElement('spanners');
+
+    li.setAttribute('data-id', doc.id);
+    showName.textContent = doc.data().showName;
+    showDate.textContent = doc.data().showDate;
+    editTickets.textContent = 'Enter Tickets';
+
+    //Creating element from the render list.
+    //updating the list of objects.
+    li.appendChild(showName);
+    li.appendChild(showDate);
+    li.appendChild(editTickets);
+    showList.appendChild(li);
+
+
+    editTickets.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let showId = e.target.parentElement.getAttribute('data-id');
+
+        var newWindow = window.open("newTicket.html");
+        newWindow.newShowid = showId;
+    });
+
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    var showName = form.showName.value;
-    db.collection(showName).set();
+    db.collection('Shows').add({
+        showName: form.showName.value,
+        showDate: form.showDate.value
     
-
-    // db.collection('showsList').add({
-    //     firstName: form.firstName.value,
-    //     lastName: form.lastName.value,
-    //     phoneNumber: form.phoneNumber.value,
-    //     email: form.email.value,
-    //     totalAttend: form.totalAttend.value,
-    //     extraInfo: form.extraInfo.value
-    // });
-    // form.firstName.value = '';
-    // form.lastName.value = '';
-    // form.phoneNumber.value = '';
-    // form.email.value = '';
-    // form.totalAttend = '';
-    // form.extraInfo = '';
+    });
+   
+    form.showName.value = '';
+    form.showDate.value = '';
 });
 
 
-
+db.collection('Shows').orderBy('showDate').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderList(change.doc);
+        }else if (change.type == 'removed'){
+            let li = showList.querySelector('[data-id=' + change.doc.id + ']');
+            showList.removeChild(li);
+        }
+    });
+});
