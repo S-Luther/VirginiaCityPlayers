5dc9f3b1b6ae645ee7d1c98093e4ca3bcab249a5
Sam Luther <samluther998@gmail.com>
2021-03-19 09:54:12 -0600

    Testing entire renderlist function

--- public/assets/js/renderlist.js
+++ public/assets/js/renderlist.js
@@ -7,7 +7,30 @@ function renderShowList(doc){
          showName = document.createElement('spanner');
          showDate = document.createElement('spanner');
          editTickets = document.createElement('spanners');  
-    li.setAttribute('data-id', doc.id);
+        li.setAttribute('data-id', doc.id);
+
+
+
+
+        showName.textContent = doc.data().showName;
+        showDate.textContent = doc.data().showDate;
+        editTickets.textContent = 'Enter Tickets';
+
+        //Creating element from the render list.
+        //updating the list of objects.
+        li.appendChild(showName);
+        li.appendChild(showDate);
+        li.appendChild(editTickets);
+        showList.appendChild(li);
+
+
+        editTickets.addEventListener('click', (e) => {
+            e.stopPropagation();
+            let showId = e.target.parentElement.getAttribute('data-id');
+
+            var newWindow = window.open("newTicket.html");
+            newWindow.newShowid = showId;
+        });
 
     } catch (e) {
         console.log("Could be testing")
@@ -16,27 +39,6 @@ function renderShowList(doc){
         }
         
     }
-
-
-    showName.textContent = doc.data().showName;
-    showDate.textContent = doc.data().showDate;
-    editTickets.textContent = 'Enter Tickets';
-
-    //Creating element from the render list.
-    //updating the list of objects.
-    li.appendChild(showName);
-    li.appendChild(showDate);
-    li.appendChild(editTickets);
-    showList.appendChild(li);
-
-
-    editTickets.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let showId = e.target.parentElement.getAttribute('data-id');
-
-        var newWindow = window.open("newTicket.html");
-        newWindow.newShowid = showId;
-    });
 
 }
 
@@ -58,45 +60,44 @@ function renderTicketList(doc){
          cross       = document.createElement('spanner');
          extraInfo   = document.createElement('div');
          li.setAttribute('data-id', doc.id);
-   } catch (e) {
-       console.log("Could be testing")
-       if (e instanceof ReferenceError) {
+
+    
+    
+        firstName.textContent = doc.data().firstName;
+        lastName.textContent = doc.data().lastName;
+        totalAttend.textContent = "# of tickets: " + doc.data().totalAttend;
+        email.textContent =  doc.data().email;
+        phoneNumber.textContent = doc.data().phoneNumber;
+        cross.textContent = 'X';
+        extraInfo.textContent = 'Additional Info: '+ doc.data().extraInfo;
+
+
+        //Creating element from the render list.
+        //updating the list of objects.
+        li.appendChild(firstName);
+        li.appendChild(lastName);
+        li.appendChild(email);
+        li.appendChild(phoneNumber);
+        li.appendChild(cross);
+        li.appendChild(totalAttend);
+        li.appendChild(extraInfo);
+        ticketList.appendChild(li);
+        //Updating the list
+
+
+        //Deleting Data
+        cross.addEventListener('click', (e) => {
+            e.stopPropagation();
+            let id = e.target.parentElement.getAttribute('data-id');
+            db.collection('test').doc(id).delete();
+
+        });
+    } catch (e) {
+        console.log("Could be testing")
+        if (e instanceof ReferenceError) {
         return true
     }
-   }
-
-    
-    
-    firstName.textContent = doc.data().firstName;
-    lastName.textContent = doc.data().lastName;
-    totalAttend.textContent = "# of tickets: " + doc.data().totalAttend;
-    email.textContent =  doc.data().email;
-    phoneNumber.textContent = doc.data().phoneNumber;
-    cross.textContent = 'X';
-    extraInfo.textContent = 'Additional Info: '+ doc.data().extraInfo;
-
-
-    //Creating element from the render list.
-    //updating the list of objects.
-    li.appendChild(firstName);
-    li.appendChild(lastName);
-    li.appendChild(email);
-    li.appendChild(phoneNumber);
-    li.appendChild(cross);
-    li.appendChild(totalAttend);
-    li.appendChild(extraInfo);
-    ticketList.appendChild(li);
-    //Updating the list
-
-
-    //Deleting Data
-    cross.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('test').doc(id).delete();
-
-    });
-    //Deleting Data
+    }
 
 }
 
