7f532dc005d62b3c302da5a4624b2ba395edc780
Nicholas <nicholaszetterberg@gmail.com>
2021-03-21 17:12:33 -0600

    Added update functionality

--- public/assets/js/newTicketapp.js
+++ public/assets/js/newTicketapp.js
@@ -6,65 +6,6 @@ const form = document.querySelector('#add-ticket-form');
 var newshowId = showId;
 
 
-<<<<<<< Updated upstream
-//For use on newTicket.html page
-//Retrieving Data from firestore
-//create element and render the list.
-function renderList(doc){
-
-    let li = document.createElement('li');
-    let firstName = document.createElement('spanner');
-    let lastName = document.createElement('spanner');
-    let totalAttend = document.createElement('spanner');
-    let email = document.createElement('spanner');
-    let phoneNumber = document.createElement('spanner');
-    let cross = document.createElement('spanner');
-    let extraInfo = document.createElement('div');
-    
-    li.setAttribute('data-id', doc.id);
-    firstName.textContent = doc.data().firstName;
-    lastName.textContent = doc.data().lastName;
-    totalAttend.textContent = "# of tickets: " + doc.data().totalAttend;
-    email.textContent =  doc.data().email;
-    phoneNumber.textContent = doc.data().phoneNumber;
-    cross.textContent = 'X';
-    extraInfo.textContent = 'Additional Info: '+ doc.data().extraInfo;
-
-
-    //Creating element from the render list.
-    //updating the list of objects.
-    li.appendChild(firstName);
-    li.appendChild(lastName);
-    li.appendChild(email);
-    li.appendChild(phoneNumber);
-    li.appendChild(cross);
-    li.appendChild(totalAttend);
-    li.appendChild(extraInfo);
-    ticketList.appendChild(li);
-    //Updating the list
-
-
-    //Deleting Data
-    cross.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('test').doc(id).delete();
-
-    });
-    //Deleting Data
-
-}
-
-// db.collection('test').orderBy('firstName').get().then((snapshot) => {
-//     snapshot.docs.forEach(doc => {
-//         renderList(doc);
-
-//     });
-
-// });
-
-=======
->>>>>>> Stashed changes
 //saving data
 form.addEventListener('submit', (e) => {
     e.preventDefault();
--- public/assets/js/renderlist.js
+++ public/assets/js/renderlist.js
@@ -1,23 +1,13 @@
 
 
 function renderShowList(doc){
-<<<<<<< Updated upstream
-
-    let li, showName, showDate, editTickets
-=======
     let li, showName, showDate, enterTickets
->>>>>>> Stashed changes
     try {
          li = document.createElement('li');
          showName = document.createElement('spanner');
          showDate = document.createElement('spanner');
-<<<<<<< Updated upstream
-         editTickets = document.createElement('spanners');  
-    li.setAttribute('data-id', doc.id);
-=======
          enterTickets = document.createElement('spanners');  
         li.setAttribute('data-id', doc.id);
->>>>>>> Stashed changes
 
     } catch (error) {
         console.log("Could be testing")
@@ -26,23 +16,6 @@ function renderShowList(doc){
     }
 
 
-<<<<<<< Updated upstream
-    showName.textContent = doc.data().showName;
-    showDate.textContent = doc.data().showDate;
-    editTickets.textContent = 'Enter Tickets';
-
-    //Creating element from the render list.
-    //updating the list of objects.
-    li.appendChild(showName);
-    li.appendChild(showDate);
-    li.appendChild(editTickets);
-    showList.appendChild(li);
-
-
-    editTickets.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let showId = e.target.parentElement.getAttribute('data-id');
-=======
         showName.textContent = doc.data().showName;
         showDate.textContent = doc.data().showDate;
         enterTickets.textContent = 'Enter Tickets';
@@ -58,7 +31,6 @@ function renderShowList(doc){
         enterTickets.addEventListener('click', (e) => {
             e.stopPropagation();
             let showId = e.target.parentElement.getAttribute('data-id');
->>>>>>> Stashed changes
 
         var newWindow = window.open("newTicket.html");
         newWindow.showId = showId;
@@ -92,38 +64,6 @@ function renderTicketList(doc){
 
     
     
-<<<<<<< Updated upstream
-    firstName.textContent = doc.data().firstName;
-    lastName.textContent = doc.data().lastName;
-    totalAttend.textContent = "# of tickets: " + doc.data().totalAttend;
-    email.textContent =  doc.data().email;
-    phoneNumber.textContent = doc.data().phoneNumber;
-    cross.textContent = 'X';
-    extraInfo.textContent = 'Additional Info: '+ doc.data().extraInfo;
-
-
-    //Creating element from the render list.
-    //updating the list of objects.
-    li.appendChild(firstName);
-    li.appendChild(lastName);
-    li.appendChild(email);
-    li.appendChild(phoneNumber);
-    li.appendChild(cross);
-    li.appendChild(totalAttend);
-    li.appendChild(extraInfo);
-    ticketList.appendChild(li);
-    //Updating the list
-
-
-    //Deleting Data
-    cross.addEventListener('click', (e) => {
-        e.stopPropagation();
-        let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('test').doc(id).delete();
-
-    });
-    //Deleting Data
-=======
         firstName.textContent = doc.data().firstName;
         lastName.textContent = doc.data().lastName;
         totalAttend.textContent = "# of tickets: " + doc.data().totalAttend;
@@ -165,7 +105,6 @@ function renderTicketList(doc){
 
         });
 
->>>>>>> Stashed changes
 
 }
 
