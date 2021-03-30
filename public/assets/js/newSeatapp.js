4e403d7ec72470877ff5922f40a3c68f21eb7699
Caleb <caleb7hayes@gmail.com>
2021-03-30 11:17:48 -0600

    print data

--- public/assets/js/newSeatapp.js
+++ public/assets/js/newSeatapp.js
@@ -5,7 +5,8 @@ console.log("testing");
 
 db.collection('test').get().then((querySnapShot) => {
     querySnapShot.forEach((doc) => {
-        console.log(doc.data());
+        console.log(doc.data()['Seat']);
+        //document.getElementById().style.fill = "red";
     });
 });
 
