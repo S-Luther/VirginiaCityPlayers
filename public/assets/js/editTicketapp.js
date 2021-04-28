e8f26181ae3c87b14227e7b8440d8376dd8e57a8
Nicholas <nicholaszetterberg@gmail.com>
2021-04-28 10:42:45 -0600

    Update editTicketapp.js

--- public/assets/js/editTicketapp.js
+++ public/assets/js/editTicketapp.js
@@ -5,7 +5,6 @@ const form = document.querySelector('#edit-ticket-form');
 var newticketId;
 
 
-//Another issue is currently you cannot close the window aafter editing the information.
 
 //saving data
 form.addEventListener('submit', (e) => {
@@ -16,6 +15,7 @@ form.addEventListener('submit', (e) => {
     if (r == true){
         e.preventDefault();
         console.log("started");
+
     
         db.collection('test').doc(newticketId).update({
             
